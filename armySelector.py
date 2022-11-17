ab81fd14ba7c451ff0cefd8585a34d47a03c5599
CaptainStinkRat <116904070+CaptainStinkRat@users.noreply.github.com>
2022-11-17 16:44:05 -0600

    Update armySelector.py

--- armySelector.py
+++ armySelector.py
@@ -11,6 +11,8 @@ class Menu(tk.Tk):
         self.teamOption = tk.StringVar(self)
         self.unitOption = tk.StringVar(self)
         self.vehicleOption = tk.StringVar(self)
+        self.vehicleAmount = tk.IntVar(self)
+        self.unitAmount = tk.IntVar(self)
         self.unitSelector = ('Foot solider','Pilot','Engineer')
         self.vehicleSelector = ('Tank','Jeep','Airplane')
         self.createWidgets()
@@ -35,24 +37,35 @@ class Menu(tk.Tk):
         vehicleButton.grid(column=3,row=0,sticky=tk.W,**paddings)
         self.outputLabel = ttk.Label(self,foreground='red')
         self.outputLabel.grid(column=0,row=1,sticky=tk.W,**paddings)
-
+        self.vehicleOutputLabel = ttk.Label(self,foreground='black')
+        self.vehicleOutputLabel.grid(column=3,row=3,sticky=tk.W,**paddings)
+        self.unitOutputLabel = ttk.Label(self,foreground='black')
+        self.unitOutputLabel.grid(column=3,row=2,sticky=tk.W,**paddings)
     def option_changed(self,*args):
         self.outputLabel['text']=f'You selected: {self.teamOption.get()}'
 
     def unitsAdd(self,*args):
         unitsToAdd = ttk.OptionMenu(self,self.unitOption,self.unitSelector[0],*self.unitSelector,command =self.units )
         unitsToAdd.grid(column=0, row=2,sticky=tk.W)
-        unitSlider = tk.Scale(self, from_=0, to=500, orient='horizontal')
+        unitSlider = tk.Scale(self, from_=0, to=500, orient='horizontal',variable=self.unitAmount)
         unitSlider.grid(column=1,row=2,stick=tk.W)
+        unitSet = ttk.Button(self,text='Set',command=self.unitSet)
+        unitSet.grid(column=3,row=2,sticky=tk.W)
     def units(self,*args):
         pass
     def vehicleAdd(self,*args):
         vehicleToAdd = ttk.OptionMenu(self,self.vehicleOption,self.vehicleSelector[0],*self.vehicleSelector,command = self.vehicles)
         vehicleToAdd.grid(column=0,row=3,sticky=tk.W)
-        vehicleSlider = tk.Scale(self, from_=0, to=500, orient='horizontal')
-        vehicleSlider.grid(column=1,row=3,stick=tk.W)
+        vehicleSlider = tk.Scale(self, from_=0, to=500,orient='horizontal',variable=self.vehicleAmount)
+        vehicleSlider.grid(column=1,row=3,sticky=tk.W)
+        vehicleSet = ttk.Button(self,text='Set',command=self.vehicleSet)
+        vehicleSet.grid(column=1,row=4,sticky=tk.W)
     def vehicles(self,*args):
         pass
+    def vehicleSet(self,*args):
+        self.vehicleOutputLabel['text']=f'{self.vehicleAmount.get()} {self.vehicleOption.get()}'
+    def unitSet(self,*args):
+        self.unitOutputLabel['text']=f'{self.unitAmount.get()} {self.unitOption.get()}'
 if __name__ == "__main__":
     app = Menu()
     app.mainloop()
