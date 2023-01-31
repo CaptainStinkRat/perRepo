320daa6abc4862eaec3bbb626b044b057a4c4f48
CaptainStinkRat <116904070+CaptainStinkRat@users.noreply.github.com>
2023-01-31 10:55:37 -0600

    Update DnDCharacterMaker.py

--- DnDCharacterMaker.py
+++ DnDCharacterMaker.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from PIL import ImageTk, Image
 
 class Menu(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.geometry('400x320')
+        self.geometry('560x320')
         self.title('DnD Character Maker')
         # self.style = ttk.Style()
         # self.style.theme_use('clam')
@@ -13,13 +14,17 @@ class Menu(tk.Tk):
         self.classSelection = tk.StringVar(self)
         self.classSelector = ['Monk','Barbarian','Archer','Rogue','Mage','Warlock','Paladin','Bard']
         self.speciesSelection = tk.StringVar(self)
-        self.speciesSelector = ['Elf','Human','Goblin','Gnome','Halfing','Dwarf','Dragonborn',]
-
+        self.speciesSelector = ['Elf','Human','Gnome','Halfling','Dwarf']
+        self.dwarfPath = 'dwarf.jpg'
+        self.humanPath = 'human.jpg'
+        self.elfPath = 'elf.jpg'
+        self.gnomePath = 'gnome.jpg'
+        self.halflingPath = 'halfling.jpg'
         self.createWidgets()
     def createWidgets(self):
         self.searchEntry = ttk.Entry(self,textvariable=self.searchTerm,width=30)
         self.searchEntry.grid(row=0,column=0)
-        self.speciesSelect = ttk.OptionMenu(self,self.speciesSelection,self.speciesSelector[0],*self.speciesSelector)
+        self.speciesSelect = ttk.OptionMenu(self,self.speciesSelection,self.speciesSelector[0],*self.speciesSelector,command=self.speciesPick)
         self.speciesSelect.grid(row=0,column=1)
         self.classSelect = ttk.OptionMenu(self,self.classSelection,self.classSelector[0],*self.classSelector,command=self.classPicks)
         self.classSelect.grid(row=0,column=2)
@@ -49,6 +54,11 @@ class Menu(tk.Tk):
         self.charismaLabel.grid(row=2,column=2)
         self.charismaInsert = ttk.Entry(self.statsLabelFrame,width=2)
         self.charismaInsert.grid(row=2,column=3)
+        self.image = ImageTk.PhotoImage(Image.open(self.elfPath))
+        self.panel = tk.Label(self,image = self.image)
+        self.panel.grid(row=1,column=1)
+        self.createButton = ttk.Button(self,text='Create character!')
+        self.createButton.grid(row=2,column=0)
 
     def classPicks(self,*args):
         if self.classSelection.get() == 'Monk':
@@ -144,6 +154,27 @@ class Menu(tk.Tk):
             self.constitutionInsert.insert(0,7)
             self.wisdomInsert.insert(0,9)
             self.charismaInsert.insert(0,12)
+    def speciesPick(self,*args):
+        if self.speciesSelection.get() == 'Elf':
+            self.image = ImageTk.PhotoImage(Image.open(self.elfPath))
+            self.panel = tk.Label(self,image = self.image)
+            self.panel.grid(row=1,column=1)
+        elif self.speciesSelection.get() == 'Dwarf':
+            self.image = ImageTk.PhotoImage(Image.open(self.dwarfPath))
+            self.panel = tk.Label(self,image = self.image)
+            self.panel.grid(row=1,column=1)
+        elif self.speciesSelection.get() == 'Human':
+            self.image = ImageTk.PhotoImage(Image.open(self.humanPath))
+            self.panel = tk.Label(self,image = self.image)
+            self.panel.grid(row=1,column=1)
+        elif self.speciesSelection.get() == 'Halfling':
+            self.image = ImageTk.PhotoImage(Image.open(self.halflingPath))
+            self.panel = tk.Label(self,image = self.image)
+            self.panel.grid(row=1,column=1)
+        elif self.speciesSelection.get() == 'Gnome':
+            self.image = ImageTk.PhotoImage(Image.open(self.gnomePath))
+            self.panel = tk.Label(self,image = self.image)
+            self.panel.grid(row=1,column=1)
 if __name__=='__main__':
     app = Menu()
     app.mainloop()
